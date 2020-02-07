9c301fed0577870729d4e86c879c0572ac2fbb08
Sibusiso1995 <54172627+Sibusiso1995@users.noreply.github.com>
2020-02-07 15:28:35 +0200

    Final commit

--- String_Calculator/string_calc.py
+++ String_Calculator/string_calc.py
@@ -1,22 +1,48 @@
 import re
 
 def add(number_str): 
+    number_str = r'{}'.format(number_str)
     if len(number_str) == 0:
         return 0
                  
-    elif len(number_str) == 1:
+    if len(number_str) == 1:
         return int(number_str)
-   
-    elif number_str[0] == "/":
-         result = 0
-         delim = number_str[3]
 
     else:
+        # extracting all numbers from the string input, both positive and negative
+        num_l = re.findall(r"-?\d+", number_str)
         result = 0
-        delim = ","
-        if number_str[1] != ",":
-            delim = "\n"
-        numbers = number_str.split(delim)
-        for num in numbers:
-            result += int(num)
+        print(num_l)
+        # extracting negative numbers from num_l
+        neg_nums = []
+        for num in num_l:
+            if int(num) < 0:
+                neg_nums.append(num)
+        # raise exception with negative numbers
+        if len(neg_nums) != 0:
+            exception_msg = 'ERROR: negatives not allowed '
+            for pos in range(len(neg_nums)):
+                if pos != len(neg_nums) - 1:
+                    exception_msg = exception_msg + neg_nums[pos] + ','
+                else:
+                    exception_msg = exception_msg + neg_nums[pos]
+            raise Exception(exception_msg)
+        for num in num_l:
+            if int(num) < 1000:
+                result = result + int(num)
         return result
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- String_Calculator/test_calculator.py
+++ String_Calculator/test_calculator.py
@@ -17,25 +17,33 @@ def test_more_than_two_elements():
 
 # handle new lines between integers
 def test_delimeter_exception():
-    with pytest.raises(Exception):
-        assert (add("1\n2,3")) == 6
+    assert (add("1\n2,3")) == 6
 
 # handle different delimeters
 def test_first_case_delimeter_exception():
-    with pytest.raises(Exception):
-        assert(add("//;\1;2")) == 3
-
-def test_second_case_delimeter_exception():
-    with pytest.raises(Exception):
-        assert(add("//4\n142")) == 3
-
-# handle negative numbers
-#def test_negative_num():
-    #if not re.match("1,2,-3,-4"):
-        #raise Exception("negatives not allowed")
+    assert(add("//;\n1;2")) == 3
 
 def test_negative_num_exception():
     with pytest.raises(Exception) as negative:
         assert(negative.add("-1,-2,3,4")) == "ERROR: negatives not allowed -1,-2"
+
+def test_greater_than_1000():
+    assert(add("//;\n1000,1;2")) == 3
+
+def test_delimeters_of_any_length():
+    assert (add("//***\n1***2***3")) == 6
+
+def test_different_delimeters():
+    assert(add("//[:D][%]\n1:D2%3")) == 6
+
+def test_different_delimeters():  
+    assert(add("//[***][%%%]\n1***2%%%3")) == 6
+
+def test_different_delimeters():    
+   assert(add("//[(-_-')][%]\n1(-_-')2%3")) == 6
+
+
+
+
 
 
